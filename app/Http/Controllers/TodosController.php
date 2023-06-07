1e7da5d2dba30df9613086de82fddc3116b3f743
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-07 16:19:21 -0400

    New function: se crearon tres nuevas funciones, show,update y destroy

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -22,9 +22,37 @@ class TodosController extends Controller
 
         return redirect()->route('todos')->with('success', 'Tarea creada correctamente');
     }
+
     public function index()
     {
         $todos = Todo::all();
         return view('todos.index', ['todos' => $todos]);
+    }
+
+    public function show($id)
+    {
+        $todo = Todo::find($id);
+        return view('todos.show', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->save();
+
+        //Ambos codigos tanto view como redirect son validos, pero hay que tener en cuenta que view necesita $todos = Todo::all();
+        //$todos = Todo::all();
+        //return view('todos.index', ['todos' => $todos, 'successs' => 'Tarea actualizada']);
+
+        return redirect()->route('todos')->with('success', 'Tarea actualizada');
+    }
+
+    public function destroy($id)
+    {
+        $todo = Todo::find($id);
+        $todo->delete();
+
+        return redirect()->route('todos')->with('success', 'Tarea se ha eliminado');
     }
 }
