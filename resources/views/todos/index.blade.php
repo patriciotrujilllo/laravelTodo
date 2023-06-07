1593a51929634964e5cbd842ae0f35eface674f1
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-07 16:18:37 -0400

    Fixed: se corrigio error de nombre

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -22,7 +22,7 @@
             @foreach ($todos as $todo)
                 <div class="row py-1">
                     <div class="col-md-9 d-flex align-items-center">
-                        <a href="{{ route('todos-edit' , ['id' => $todo->id]) }}">{{ $todo->title}}</a>
+                        <a href="{{ route('todo-edit' , ['id' => $todo->id]) }}">{{ $todo->title}}</a>
                     </div>
 
                     <div class="col-md-3 d-flex justify-content-end">
