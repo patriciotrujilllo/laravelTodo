c179bfe2daf8c145b8cae77f33446847f5bdca3c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-07 20:39:11 -0400

    Se creo la vista para las categorias extendiendo app y agregando la seccion de 'content', se agrego un text y un color

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,32 @@
+@extends('app')
+
+@section('content')
+    <div class="container w-25 border p-4 my-4">
+        <div class="row mx-auto">
+            <form action="{{route('categories.store')}}" method="POST">
+                @csrf
+                
+                @if (session('success'))
+                    <h6 class="alert alert-success">{{ session('success')}}</h6>
+                @endif
+                
+                @error('name')
+                    <h6 class="alert alert-danger">{{ $message }}</h6>
+                @enderror
+                    
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nombre de la categoria</label>
+                    <input type="text" class="form-control" name="name">
+                </div>
+
+                <div class="mb-3">
+                    <label for="color" class="form-label">Color de la categoria</label>
+                    <input type="color" class="form-control" name="color">
+                </div>
+
+                <button type="submit" class="btn btn-primary mt-2">Crear nueva tarea</button>
+            </form>
+        </div>
+
+    </div>
+@endsection
