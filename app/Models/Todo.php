df3b30fec3de77b1198f3646c25fd725a032753a
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-08 21:26:31 -0400

    Se agrega e el modelo la relacion que existe con la categoria, una tarea solo puede tener asociada una categoria

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Todo extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title'];
+
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
