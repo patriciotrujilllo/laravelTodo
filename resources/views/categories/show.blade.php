7b0a76540a2f73d2f5857b5957986c96a213655c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-08 21:31:31 -0400

    Se agrego la posibilidad de ver la categoria para editarla y ademas la lista con todas las tareas de esa categoria y la posibilidad de eliminar a la categoria en conjunto con cada tarea asociada(debido a la relacion entre las tablas)

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,5 +1,57 @@
 @extends('app')
 
 @section('content')
-    Holaaaaa
+    <div class="container w-25 border p-4 my-4">
+        <div class="row mx-auto">
+            <form action="{{route('categories.update',[$category->id])}}" method="POST">
+                @method('PATCH')
+                @csrf
+                
+                @if (session('success'))
+                    <h6 class="alert alert-success">{{ session('success')}}</h6>
+                @endif
+                
+                @error('name')
+                    <h6 class="alert alert-danger">{{ $message }}</h6>
+                @enderror
+                    
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nombre de la categoria</label>
+                    <input type="text" class="form-control" name="name" value="{{$category->name}}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="color" class="form-label">Color de la categoria</label>
+                    <input type="color" class="form-control" name="color" value="{{$category->color}}">
+                </div>
+
+                <button type="submit" class="btn btn-primary mt-2">Actualizar categoria</button>
+            </form>
+        </div>
+
+        @if ($category->todos->count()>0)
+            @foreach ($category->todos as $todo)
+                <div class="row py-1">
+                    <div class="col-md-9 d-flex align-items-center">
+                        <a href="{{ route('todo-edit' , ['id' => $todo->id]) }}">{{ $todo->title}}</a>
+                    </div>
+
+                    <div class="col-md-3 d-flex justify-content-end">
+                        <form action="{{ route('todos-destroy' , [$todo->id]) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button class="btn btn-danger btn-sm">Eliminar</button>
+                        </form>
+                    </div>
+
+                </div>
+            @endforeach  
+            
+        @else
+            No hay tareas en la categoria
+        @endif
+
+        
+        
+    </div>
 @endsection
